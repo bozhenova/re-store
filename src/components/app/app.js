152232bcc9e7c4664fa543d6b6bac3bc471a5bf0
Margo <margo.bozhenova@gmail.com>
2020-06-21 01:26:52 +0300

    Style the app

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './app.css';
+import ShopHeader from '../shop-header';
 import { Route, Switch } from 'react-router-dom';
 import { HomePage, CartPage } from '../pages';
 
 const App = () => {
   return (
-    <Switch>
-      <Route exact path='/' component={HomePage} />
-      <Route path='/cart' component={CartPage} />
-    </Switch>
+    <main role='main' className='container'>
+      <ShopHeader />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/cart' component={CartPage} />
+      </Switch>
+    </main>
   );
 };
 
