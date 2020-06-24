9b7cdf53f88dac7dcff036f1febe0ba9f1320140
Margo <margo.bozhenova@gmail.com>
2020-06-25 00:19:57 +0300

    Updates

---
+++ precache-manifest.7130d3f7c106b10f3b028372fc816b06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e39996b3e55109969646118f5a3bfb97",
+    "url": "/bozhenova/re-store/index.html"
+  },
+  {
+    "revision": "590f20112e2f9e29e56c",
+    "url": "/bozhenova/re-store/static/css/main.3ac70ef1.chunk.css"
+  },
+  {
+    "revision": "94fd36314db975a2d57a",
+    "url": "/bozhenova/re-store/static/js/2.63da2809.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bozhenova/re-store/static/js/2.63da2809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "590f20112e2f9e29e56c",
+    "url": "/bozhenova/re-store/static/js/main.8d128c31.chunk.js"
+  },
+  {
+    "revision": "bdd6d3c47a1a5a48cebd",
+    "url": "/bozhenova/re-store/static/js/runtime-main.f887c07a.js"
+  }
+]);
